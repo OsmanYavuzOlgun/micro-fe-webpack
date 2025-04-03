ae47cadaefe074e8ed1b40c7bab5d55f0a2c83a7
yavuzolguntg <osmanyavuzolgun@gmail.com>
2025-04-03 14:21:27 +0300

    micro-fe-webpack:dummy

---
+++ micro-fe-vite-webpack/wp-host/src/Dummy.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import React from "react";
+
+export const Dummy = (props) => {
+  return (
+    <div>
+      <h5>DUMMY CONTAINER (Webpack)</h5>
+    </div>
+  );
+};
+
+export default Dummy;
